c6c938d9ad3aaf2edda72d6ba67be0586167dd2b
Chiara Aengevelt <chiara@aengevelt.com>
2021-01-09 17:09:23 +0100

    adding view for all articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # show all articles
+  get "articles", to: "articles#index"
+
+  # create new article - 2 Steps GET and POST
+  get "articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
+
+  # show one article
+  get "articles/:id", to: "articles#show", as: :article
 end
